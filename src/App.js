bee37720e49fda97b470298a6c4d1116c390c3a7
nacht-falter <johannes.bernet@posteo.de>
2023-09-27 15:56:59 +0200

    feat: setup app routes, add active links to navigation (#33)
    Add routes to App component, add active link class and styles
    to BottomNavBar.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import TopNavBar from "./components/TopNavBar";
 import BottomNavBar from "./components/BottomNavBar";
 import styles from "./App.module.css";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
       <TopNavBar />
+      <Container>
+        <Switch>
+          <Route exact path="/" render={() => {}} /> 
+          <Route exact path="/signin" render={() => {}} />
+          <Route exact path="/signup" render={() => {}} />
+          <Route exact path="/feed" render={() => {}} />
+          <Route exact path="/map" render={() => {}} />
+          <Route exact path="/favourites" render={() => {}} />
+          <Route exact path="/news" render={() => {}} />
+          <Route render={() => <h1>404 Not Found</h1>} />
+        </Switch>
+      </Container>
       <BottomNavBar />
     </div>
   );
--- src/components/BottomNavBar.js
+++ src/components/BottomNavBar.js
@@ -7,31 +7,31 @@ const BottomNavBar = () => {
   return (
     <Navbar className={styles.BottomNavBar} fixed="bottom">
       <Container className={styles.Container}>
-        <NavLink to="/">
+        <NavLink exact to="/" activeClassName={styles.Active}>
           <Stack className="text-center">
             <i className="fa-solid fa-magnifying-glass-location"></i>
             <span className={styles.NavItemText}>Explore</span>
           </Stack>
         </NavLink>
-        <NavLink to="/">
+        <NavLink to="/feed" activeClassName={styles.Active}>
           <Stack className="text-center">
             <i className="fa-solid fa-radio"></i>
             <span className={styles.NavItemText}>Feed</span>
           </Stack>
         </NavLink>
-        <NavLink to="/">
+        <NavLink to="/map" activeClassName={styles.Active}>
           <Stack className="text-center">
             <i className="fa-solid fa-map-location"></i>
             <span className={styles.NavItemText}>Map</span>
           </Stack>
         </NavLink>
-        <NavLink to="/">
+        <NavLink to="/favourites" activeClassName={styles.Active}>
           <Stack className="text-center">
             <i className="fa-solid fa-heart-pulse"></i>
             <span className={styles.NavItemText}>Favourites</span>
           </Stack>
         </NavLink>
-        <NavLink to="/">
+        <NavLink to="/news" activeClassName={styles.Active}>
           <Stack className="text-center">
             <i className="fa-solid fa-bell"></i>
             <span className={styles.NavItemText}>News</span>
