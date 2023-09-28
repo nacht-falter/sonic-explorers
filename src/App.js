df1767ec0a836d9aeb47ed5447e6824b645b943c
nacht-falter <johannes.bernet@posteo.de>
2023-09-28 14:26:41 +0200

    feat: add signin form component (#2)
    Add and import SignInForm component. Use axios to
    make requests to the API for logging in.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
       <Container className={styles.Main}>
         <Switch>
           <Route exact path="/" render={() => {}} />
-          <Route exact path="/signin" render={() => {}} />
+          <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/feed" render={() => {}} />
           <Route exact path="/map" render={() => {}} />
---
+++ src/pages/auth/SignInForm.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Alert from "react-bootstrap/Alert";
+import styles from "../../styles/Authentication.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import hr from "../../assets/images/hr-wave.svg";
+import image from "../../assets/images/bruno-aguirre-wOWgGnf1Gng-unsplash.jpg";
+import axios from "axios";
+
+const SignInForm = () => {
+  const [signInData, setSignInData] = useState({ username: "", password: "" });
+  const { username, password } = signInData;
+  const [errors, setErrors] = useState({});
+  const history = useHistory();
+
+  const handleChange = (e) => {
+    setSignInData({
+      ...signInData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/login/", signInData);
+      history.push("/");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
+  return (
+    <Row className="mx-auto">
+      <Col className="my-auto py-2 p-md-4 p-lg-5" md={6}>
+        <h2>Welcome back</h2>
+        <p>Continue your sonic explorations</p>
+        <img className="d-none d-md-inline" src={image} alt="Woman holding a mobile phone" width="100%" />
+        <img className="d-inline d-md-none" src={hr} alt="Horizontal line with sound wave" width="100%" />
+      </Col>
+      <Col className="my-auto py-2 p-md-4 p-lg-5" md={6}>
+        <Container>
+          <Form onSubmit={handleSubmit}>
+            <FloatingLabel className={styles.Label} controlId="floatingInput" label="Username">
+              <Form.Control
+                className={styles.Input}
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
+            </FloatingLabel>
+
+            {errors.username?.map((msg, i) => (
+              <Alert variant="warning" key={i}>
+                {msg}
+              </Alert>
+            ))}
+
+            <FloatingLabel className={styles.Label} controlId="password" label="Password">
+              <Form.Control
+                className={styles.Input}
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+              />
+            </FloatingLabel>
+
+            {errors.password?.map((msg, i) => (
+              <Alert variant="warning" key={i}>
+                {msg}
+              </Alert>
+            ))}
+
+            {errors.non_field_errors?.map((msg, i) => (
+              <Alert variant="warning" key={i}>
+                {msg}
+              </Alert>
+            ))}
+
+            <Button className={`${btnStyles.Button} ${btnStyles.Wide}`} type="submit">
+              Sign In
+            </Button>
+          </Form>
+        </Container>
+        <Container>
+          <img className="d-none d-md-inline mt-4" src={hr} alt="Horizontal line with sound wave" width="100%" />
+          <Link className={`${styles.Link} mt-4`} to="/signup">
+            Don't have an account yet? <span>Sign up</span>
+          </Link>
+        </Container>
+      </Col>
+      <hr className="d-none d-md-inline mt-5" />
+    </Row>
+  );
+};
+
+export default SignInForm;
