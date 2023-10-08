7ddb4b199b96d8f3e8e309d0b54c8ccb0c0c8af5
nacht-falter <johannes.bernet@posteo.de>
2023-10-08 23:46:28 +0200

    fix: fix sound edit form heading

--- src/pages/sounds/SoundEditForm.js
+++ src/pages/sounds/SoundEditForm.js
@@ -238,7 +238,7 @@ function SoundEditForm(props) {
         <Form onSubmit={handleSubmit}>
           <Row>
             <h3 className="mb-4">
-              Upload new sound
+              Update sound
               <OverlayTrigger
                 placement="bottom"
                 overlay={
