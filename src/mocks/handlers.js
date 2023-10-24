c8751268235f0d1461b9a8b1049fe43c58f3d074
nacht-falter <johannes.bernet@posteo.de>
2023-10-24 16:47:09 +0200

    test: configure msw for mocking api requests
    Install and configure Mock Service Worker (MSW).
    Add handlers for mocking API requests.

---
+++ src/mocks/handlers.js
@@ -0,0 +1,23 @@
+import { rest } from "msw";
+
+const baseURL = "https://sonic-explorers-api-4c187c4cd99f.herokuapp.com/";
+
+export const handlers = [
+  rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        pk: 1,
+        username: "testuser",
+        email: "",
+        first_name: "",
+        last_name: "",
+        profile_id: 1,
+        profile_image: "https://res.cloudinary.com/dmmd6fle7/image/upload/default_profile_p5rke8",
+        is_staff: true,
+      })
+    );
+  }),
+  rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+];
