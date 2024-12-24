3c32a0b8348346f96a46011bfc9b7a2154e479f6
Johannes Bernet <jofb@posteo.de>
2024-12-25 00:56:56 +0100

    Fix DRF default auth classes

--- api/sonic_explorers_api/settings.py
+++ api/sonic_explorers_api/settings.py
@@ -29,9 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         (
-            "rest_framework.authentication.SessionAuthentication"
-            if development
-            else "dj_rest_auth.jwt_auth.JWTCookieAuthentication"
+            "dj_rest_auth.jwt_auth.JWTCookieAuthentication"
         )
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
