21a20eb7182e0c45c0626c254e57534ead1ff2fa
Johannes Bernet <jofb@posteo.de>
2024-12-25 00:59:15 +0100

    Read axios base url from environment

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://sonic-explorers-api-4c187c4cd99f.herokuapp.com/";
+// Get the base URL from environment variable
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
